91473e53585a5f69f0b9859eb73fc7943eb89261
Ryoko-san <simonshiroganta@gmail.com>
2019-05-13 20:52:11 +0000

    baseDeDatos

--- api/models.py
+++ api/models.py
@@ -3,24 +3,70 @@ All your application modules and serializers are going to be declared inside thi
 """
 from rest_framework import serializers
 from django.db import models
-
-"""
-Define he Contact Entity into your applcation model
-"""
-class Contact(models.Model):
-    first_name = models.CharField(max_length=50, default='')
-    last_name = models.CharField(max_length=50, default='')
-    phone_number = models.CharField(max_length=15, default='')
-    email = models.CharField(max_length=150, default='')
-
-"""
-The ContactSerializer is where you will specify what properties
-from the ever Contact should be inscuded in the JSON response
-"""
-class ContactSerializer(serializers.ModelSerializer):
+import datetime
 
 
+class Users(models.Model):
+    __tablename__ = 'Users'
+    email = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, null=False)
+    f_name = models.CharField(max_length=250, null=False)
+    l_name = models.CharField(max_length=250, null=False)
+    role = models.CharField(max_length=250, null=False)
+    positions_id = models.ForeignKey('Positions', on_delete=models.CASCADE)
+
+class UsersSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Contact
-        # what fields to include?
-        fields = ('first_name','last_name', 'phone_number', 'email')
+        model = Users
+        fields = ('email', 'phone_number', 'f_name', 'l_name', 'role')
+
+
+class Shifts(models.Model):
+    __tablename__ = 'Shift'
+    task = models.CharField(max_length=250, null=False)
+    date = datetime.date.today()
+    users_id = models.ForeignKey('Users', on_delete=models.CASCADE)
+    shift_types_id = models.ForeignKey('Shift_types', on_delete=models.CASCADE)
+
+
+class ShiftsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shifts
+        fields = ('task', 'date')
+
+
+class Positions(models.Model):
+    __tablename__ = 'Positions'
+    position_name = models.CharField(max_length=250, null=False)
+
+
+class PositionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Positions
+        fields = ('position_name')
+
+
+class Shift_types(models.Model):
+    __tablename__ = 'Shift_types'
+    shift_name = models.CharField(max_length=250, null=False)
+    shift_start = models.DateField(max_length=250, null=False)
+    shift_end = models.DateField(max_length=250, null=False)
+
+
+class Shift_typesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shift_types
+        fields = ('shift_name', 'shift_start', 'shift_end')
+
+
+class Client(models.Model):
+    __tablename__ = 'Client'
+    client_name = models.CharField(max_length=250, null=False)
+    users_id = models.ForeignKey('Users', on_delete=models.CASCADE)
+    shifts_id = models.ForeignKey('Shifts', on_delete=models.CASCADE)
+
+
+class ClienttSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = ('lient_name')
--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.models import Contact, ContactSerializer
 
 """
 The ContactsView will contain the logic on how to:
