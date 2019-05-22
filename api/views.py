639095c3359d39f807a58f2692a097f0d55ec8df
Ryoko-san <simonshiroganta@gmail.com>
2019-05-22 19:57:16 +0000

    temp

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 import json
+from datetime import datetime
+from calendar import monthrange
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.models import UsersSerializer, Users
+from api.models import UsersSerializer, Users, Shifts, ShiftsSerializer
 
 
 """
@@ -17,14 +19,14 @@ class UsersView(APIView):
 
         users = Users.objects.all()
         serializer = UsersSerializer(users, many=True)
-        return Response(json.dumps(serializer.data))
+        return Response(serializer.data)
 
     def post(self, request):
 
         val = json.loads(request.body)
         new_user = Users.objects.create(email=val['email'], phone_number=val['phone_number'], f_name=val['f_name'], l_name=val['l_name'], role=val['role'])
         new_user.save()
-        return Response(json.dumps(val), status=status.HTTP_200_OK)
+        return Response('datos guardados', status=status.HTTP_200_OK)
 
     def delete(self, request, id):
 
@@ -35,11 +37,6 @@ class UsersView(APIView):
     def put(self, request, id):
 
         users = Users.objects.get(id=id)
-        #users_list = request.data["list"]
-        #for task in users_list:
-            #new_user = Users.objects.all()
-            #new_user.save()
-        #new_user = Users.objects.all()
         val = json.loads(request.body)
         users.email= val['email']
         users.phone_number= val['phone_number']
@@ -47,5 +44,18 @@ class UsersView(APIView):
         users.l_name= val['l_name']
         users.role= val['role']
         users.save()
-        #serializer = UsersSerializer(users, many=True)
-        return Response(json.dumps('ok'), status=status.HTTP_200_OK)
+        return Response('ok', status=status.HTTP_200_OK)
+
+
+class ShiftViews (APIView):
+
+    def get (self, request, date=None):
+
+        mrange = monthrange(int(date[:4]), int(date[5:7]))
+        lastday = str(mrange[1])
+        datesum = date+"-01"
+        datestart = datetime.strptime(datesum, "%Y-%m-%d")
+        dateend = datetime.strptime(str(date+"-"+lastday), "%Y-%m-%d")
+        shifts = Shifts.objects.filter(date_start__lte = dateend, date_end__gte = datestart)
+        serializer = ShiftsSerializer(shifts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
